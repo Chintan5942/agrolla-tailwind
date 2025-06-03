771aea1d46d486e93952e6809569031c0b1f4b1b
TrushitMarviya <marviyatrushit0@gmail.com>
2025-06-03 18:51:54 +0530

    Adjust bird position in Homepage.css and wrap Navbar in a div for layout consistency

--- src/app/HomePage/Navbar.jsx
+++ src/app/HomePage/Navbar.jsx
@@ -18,7 +18,8 @@ export default function Navbar() {
   }, []);
 
   return (
-    <nav className="w-full h-[72px] px-4 md:px-8 flex items-center justify-between lg:justify-around bg-[#ffffff] top-0 sticky z-40" data-aos="fade-down">
+    <div className="">
+          <nav className="w-[95%] h-[72px] px-4 md:px-8 flex items-center justify-between lg:justify-around bg-[#ffffff] top-0 sticky z-40" data-aos="fade-down">
       {/* Logo */}
       <a href="/" className="flex items-center" data-aos="fade-right">
         <img src="/agrolla-logo.png" alt="Agrolla Logo" className="w-[85.71px] h-[60px]" />
@@ -51,5 +52,6 @@ export default function Navbar() {
         </div>
       )}
     </nav>
+    </div>
   );
 }
